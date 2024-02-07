104538f9163b46a4e53998319add208089a49663
YUVRAJ KUMAR ARORA <110113664+Officialyuvi070@users.noreply.github.com>
2024-02-07 12:59:57 +0530

    Update AppTest.java

--- Fucntional-Testing/demo/src/test/java/com/example/AppTest.java
+++ Fucntional-Testing/demo/src/test/java/com/example/AppTest.java
@@ -29,7 +29,7 @@ public class AppTest {
         driver.get("http://localhost:3000/");
         System.out.println(driver.getTitle());
       
-            if (driver.getTitle().equals("ToDo App")){
+            if (driver.getTitle().equals("ToDoApp")){
                 
                 test.log(LogStatus.PASS, "Navigated to the specified URL");
             } else {
